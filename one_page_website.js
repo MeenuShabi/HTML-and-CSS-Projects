5f4ff09dd40e39e5598473df94a2eaa1f9019560
MeenuShabi <61444239+MeenuShabi@users.noreply.github.com>
2025-02-22 15:11:10 -0500

    Update one_page_website.js

--- one_page_website.js
+++ one_page_website.js
@@ -1,1 +1,22 @@
+// Create lightbox container
+const lightbox = document.createElement("div");
+lightbox.id = "lightbox";
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll(".gallery img");
+images.forEach((image) => {
+    image.addEventListener("click", (e) => {
+        lightbox.classList.add("active");
+        const img = document.createElement("img");
+        img.src = image.dataset.large;
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild);
+        }
+        lightbox.appendChild(img);
+    });
+});
+
+lightbox.addEventListener("click", () => {
+    lightbox.classList.remove("active");
+});
 
